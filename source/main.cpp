e5fd14c38f85d896a7982c32995d8945c683915a
jamesmcgill <jamesmcgill@users.noreply.github.com>
2019-09-19 18:29:17 +0200

    use glfw to create and open a window

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,22 @@
 #include <fmt/format.h>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 int
 main()
 {
-  fmt::print("Hello, {}!\n", "world");
+  glfwInit();
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow* window
+    = glfwCreateWindow(800, 600, "Vulkan hello triangle", nullptr, nullptr);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+  }
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
   return 0;
 }
